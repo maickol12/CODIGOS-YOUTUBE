679f5ba46a1d1f25d035b88505abf98ee043208d
maickol12 <rocm960107@outlook.com>
2017-08-18 23:05:46 -0500

    ConsumirApiRes en android

---
+++ ConsumirApiRest/app/src/main/java/com/example/mand/consumirapirest/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.mand.consumirapirest;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    TextView sal;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sal  = (TextView) findViewById(R.id.salida);
+        getData();
+    }
+
+
+    public void getData(){
+        String sql = "http://skiboo.com.mx/api/business/rewards/49";
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        URL url = null;
+        HttpURLConnection conn;
+
+        try {
+            url = new URL(sql);
+            conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("GET");
+
+            conn.connect();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String inputLine;
+
+            StringBuffer response = new StringBuffer();
+
+            String json = "";
+
+            while((inputLine = in.readLine()) != null){
+                response.append(inputLine);
+            }
+
+            json = response.toString();
+
+            JSONArray jsonArr = null;
+
+            jsonArr = new JSONArray(json);
+            String mensaje = "";
+            for(int i = 0;i<jsonArr.length();i++){
+                JSONObject jsonObject = jsonArr.getJSONObject(i);
+
+                Log.d("SLIDA",jsonObject.optString("description"));
+                mensaje += "DESCRIPCION "+i+" "+jsonObject.optString("description")+"\n";
+            }
+            sal.setText(mensaje);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
